5716db7449d1fbd0ce658fda95590b64e2b2d2e7
A Suicidal Cow <aaron.bertagnole@gmail.com>
2020-07-11 13:30:28 -0600

    Updates

---
+++ precache-manifest.48db0547ad74d4a5098a78fd82c5c8f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51649c3a4e3e4d5d3d08f4d21b140140",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "accd94736b7257e3546f",
+    "url": "/clicky-game/static/css/main.5822c4b9.chunk.css"
+  },
+  {
+    "revision": "fa16647add3b33dfa690",
+    "url": "/clicky-game/static/js/2.680c17b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.680c17b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "accd94736b7257e3546f",
+    "url": "/clicky-game/static/js/main.433f2756.chunk.js"
+  },
+  {
+    "revision": "a2dd66fdceb11b57b463",
+    "url": "/clicky-game/static/js/runtime-main.3c5f3143.js"
+  }
+]);
